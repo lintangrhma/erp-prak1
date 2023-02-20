3b3467ee6670aac3e479c091fa9e9801200dc766
lintangrhma <lintangrahma9@gmail.com>
2023-02-20 15:09:40 +0700

    upload

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+{
+    "name"           : "Lintang Rahmawati",
+    "version"        : "14.0",
+    "author"         : "Lintang Rahmawati",
+    "website"        : "https://www.sociolla.com/",
+    "category"       : "New Module",
+    "summary"        : "Ini adalah Lintang Baru",
+    "description"    : """
+        Latihan membuat addons baru di odoo 14
+    """,
+    "depends"        : [
+        "base",
+    ],
+    "data"           : [
+
+    ],
+    "demo"           : [],
+    "test"           : [],
+    "image"          : [],
+    "qweb"           : [],
+    "css"            : [],
+    "application"    : True,
+    "installable"    : True,
+    "auto_install"   : False,
+}
